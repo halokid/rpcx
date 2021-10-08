92e95c9056932b720a1ee1a2eb1793d5fdde7dbe
r0x <r00txxyyzz@gmail.com>
2021-10-08 18:47:50 +0800

    change msgpack to my repo

--- client/caller_cakerabbit.go
+++ client/caller_cakerabbit.go
@@ -6,7 +6,7 @@ implement caller for cakeRabbit service
 import (
   "github.com/halokid/ColorfulRabbit"
   "github.com/halokid/rpcx-plus/log"
-  "github.com/msgpack-rpc/msgpack-rpc-go/rpc"
+  "github.com/halokid/msgpack-rpc-go/rpc"
   "github.com/spf13/cast"
   logx "log"
   "net"
@@ -24,14 +24,16 @@ func (c *caller) invokeCake(nodeAddr string, svc string, call string, bodyTran m
   var args []interface{}
   //paramsIndex := []string{"pageIndex", "pageSize", "keyword"}
   paramsIndex := psKey
-  //for _, arg := range bodyTran {
-    //args = append(args, arg)
-    //args = append(args, cast.ToString(arg))
-  //}
 
   for _, key := range paramsIndex {
     if val, ok := bodyTran[key]; ok {
       args = append(args, cast.ToString(val))
+    }
+  }
+  // todo: 如果是非自定义路由， 则重新读取body的json数据， 不依赖于pkKey
+  if len(args) == 0 {
+    for _, arg := range bodyTran {
+      args = append(args, cast.ToString(arg))
     }
   }
 
